d0f40700ad86ad8470a2e5e21f2f12d79e36d442
Kamalyesh Kannadkar <kkamalyesh@gmail.com>
2021-11-08 02:12:00 +0530

    initial commit

---
+++ src/utils/getNextId.js
@@ -0,0 +1,11 @@
+var { getNextId } = (function () {
+    var id_counter = 0
+    function getNextId() {
+        if (id_counter < 0) return "id_0"
+        else return `id_${++id_counter}`
+    }
+    return { getNextId }
+}())
+
+
+export default getNextId
